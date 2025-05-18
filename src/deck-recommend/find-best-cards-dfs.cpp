ba84abd8294a00b7de5aa7014ef468aa89559e7d
NeuraXmy <neuraxmy@outlook.com>
2025-05-18 13:12:47 +0800

    fixed dfs bug

--- src/deck-recommend/find-best-cards-dfs.cpp
+++ src/deck-recommend/find-best-cards-dfs.cpp
@@ -81,17 +81,17 @@ void BaseDeckRecommend::findBestCardsDFS(
             bool lessThan = false;
             bool greaterThan = false;
             if (cfg.target == RecommendTarget::Score) {
-                lessThan = this->cardCalculator.isCertainlyLessThan(card, last);
-                greaterThan = this->cardCalculator.isCertainlyLessThan(last, card);
+                lessThan = this->cardCalculator.isCertainlyLessThan(last, card);
+                greaterThan = this->cardCalculator.isCertainlyLessThan(card, last);
             } else if(cfg.target == RecommendTarget::Power) {
-                lessThan = card.power.isCertainlyLessThan(last.power);
-                greaterThan = last.power.isCertainlyLessThan(card.power);
+                lessThan = last.power.isCertainlyLessThan(card.power);
+                greaterThan = card.power.isCertainlyLessThan(last.power);
             } else if (cfg.target == RecommendTarget::Skill) {
-                lessThan = card.skill.isCertainlyLessThan(last.skill);
-                greaterThan = last.skill.isCertainlyLessThan(card.skill);
+                lessThan = last.skill.isCertainlyLessThan(card.skill);
+                greaterThan = card.skill.isCertainlyLessThan(last.skill);
             }
             // 要求生成的卡组后面4个位置按强弱排序、同强度按卡牌ID排序
-            // 如果上一张卡肯定小，那就不符合顺序；在旗鼓相当的前提下（因为两两组合有四种情况，再排除掉这张卡肯定小的情况，就是旗鼓相当），要ID大
+            // 如果上一张卡肯定小，那就不符合顺序；在旗鼓相当的前提下（因为两两组合有四种情况，再排除掉这张卡肯定小的情况，就是旗鼓相当），要ID小
             if (lessThan || (!greaterThan && card.cardId > last.cardId)) {
                 continue;
             }
